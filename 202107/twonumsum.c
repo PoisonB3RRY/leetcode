7a8dcdf28763d2607238edec3e601c0449593311
nickj <nickjianjian@outlook.com>
2021-07-08 01:27:42 +0800

    leetcode 202107

--- 202107/twonumsum.c
+++ 202107/twonumsum.c
@@ -5,13 +5,13 @@ int* twoSum(int*, int, int, int*);
 
 int main(int argc, char const *argv[])
 {
-    int nums[] = {2, 7, 10, 15};
-    int numsSize = 4;
-    int target = 9;
-    int *returnSize;
+    int nums[] = {3, 2, 4};
+    int numsSize = 3;
+    int target = 6;
+    int *returnSize = malloc(sizeof(int));
 
     int *ret = twoSum(nums, numsSize, target, returnSize);
-    for (int i = 0; i < numsSize; ++i)
+    for (int i = 0; i < *returnSize; ++i)
     {
         printf("%d\n", ret[i]);
     }
@@ -27,7 +27,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int *ret = malloc(sizeof(int) * 2);
 
     for (index1 = 0; index1 < numsSize; ++index1) {
-        for (index2 = 0; index2 < numsSize; ++index2) {
+        for (index2 = index1 + 1; index2 < numsSize; ++index2) {
             if ((nums[index1] + nums[index2]) == target) {
                 ret[0] = index1;
                 ret[1] = index2;
